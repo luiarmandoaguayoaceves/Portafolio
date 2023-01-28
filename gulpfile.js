e92d94098f70ff9c3020d84aed134c68592a4583
Luis Armando Aguayo Aceves <luisarmandoaguayoaceves@gmail.com>
2023-01-28 17:49:54 -0600

    Portafolio v1.1

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,75 @@ const avif = require('gulp-avif');
 //JAVASCRIPT
 const terser = require ('gulp-terser-js');
 
+
+//CSS compilar
 function css (done){
-    src()
+    src('src/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano() ]) )
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('build/css'))
     done();
 }
+
+//Hacer mas ligera las imagenes
+function imagenes(done){
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+    .pipe(cache(imagemin(opciones)))
+    .pipe(dest('build/img'))
+    done();
+}
+
+//Convertir a webp
+function versionWebp(done){
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+    .pipe(webp(opciones))
+    .pipe(dest('build/img'))
+    done();
+}
+
+//Imagen AVIF
+function versionAvif(done){
+    const opciones = {
+        quiality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+    .pipe(avif(opciones))
+    .pipe(dest('build/img'))
+    done();
+}
+
+//Javascript minificar
+function javascript(done){
+    src('src/js/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(terser())
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('build/js'))
+    done();
+}
+
+function dev(done){
+    watch('src/scss/**/*.scss', css);
+    watch('src/js/**/*.js', javascript);
+    done();
+}
+
+exports.css = css;
+exports.js = javascript;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+
+// exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
+exports.dev = (dev);
