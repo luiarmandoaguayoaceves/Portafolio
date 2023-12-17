0752dedfb444b2f02afb4903332d11d8c5a32f04
Luis Armando Aguayo Aceves <luisarmandoaguayoaceves@gmail.com>
2023-12-17 14:34:00 -0600

    Actualizacion

--- gulpfile.js
+++ gulpfile.js
@@ -88,4 +88,4 @@ exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
 
 exports.dev = parallel(imagenes, versionWebp, versionAvif, dev, javascript);
-exports.dev = (dev);
+//exports.dev = (dev); //Usar mnientras se desarrolla y si no, comentar
