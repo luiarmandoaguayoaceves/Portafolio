36108ccb91ce49c3c68f8a3982acd3aff24e9921
Luis Armando Aguayo Aceves <luisarmandoaguayoaceves@gmail.com>
2024-01-18 11:41:57 -0600

    responsivo para telefonos

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
     navegador = document.querySelector('#nav');
     desplegarMenu = document.querySelector('#abrir');
     cerrarMenu = document.querySelector('#cerrar');
+    seleccionMenu = document.querySelectorAll('a');
+
+
 
     desplegarMenu.addEventListener('click', () => {
         navegador.classList.add('visible');
@@ -14,6 +17,16 @@ document.addEventListener('DOMContentLoaded', () => {
         navegador.classList.remove('visible');
         desplegarMenu.style.display = "block";
     })
+    
+    primerosCuatroEnlaces = Array.from(seleccionMenu).slice(0, 4);
+    seleccionMenu.forEach(seleccion => {
+        seleccion.addEventListener('click', () => {
+            navegador.classList.remove('visible');
+            desplegarMenu.style.display = "block";
+        })
+    });
+    
+
     
     function verificarTamañoVentana() {
         var tamañoVentana = window.innerWidth;
